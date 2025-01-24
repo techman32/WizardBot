b4a5ad65cb9597c4a5ea337aa689580da4293f21
alexander.klochko <klochko977@gmail.com>
2025-01-24 09:23:51 +0300

    Добавил проверку на отписку от канала

--- src/commands/search.command.ts
+++ src/commands/search.command.ts
@@ -5,23 +5,31 @@ import {getGradeButtons, showErrorMessage} from '../utils/bot.utils'
 
 export class SearchCommand extends Command {
     private readonly apiService: ApiService
+    partnerChannelId: string
 
     constructor(bot: Telegraf<Context>, apiService: ApiService) {
         super(bot)
         this.apiService = apiService
+        this.partnerChannelId = '@gdzmania'
     }
 
     handle(): void {
         this.bot.hears('🔍 Найти ответ', async (ctx) => {
             try {
-                const grades = await this.apiService.getGrades()
-                const buttons = getGradeButtons(grades)
+                const chatMember = await ctx.telegram.getChatMember(this.partnerChannelId, ctx.from.id)
 
-                await ctx.deleteMessage()
-                await ctx.reply('Выберите класс', Markup.inlineKeyboard([
-                    ...buttons,
-                    [Markup.button.callback('❌ Закрыть', 'close')]
-                ]))
+                if (['left', 'kicked'].includes(chatMember.status)) {
+                    ctx.reply('Вы не подписаны на канал! Напиши \/start и выберите "Подписаться"', Markup.removeKeyboard())
+                } else {
+                    const grades = await this.apiService.getGrades()
+                    const buttons = getGradeButtons(grades)
+
+                    await ctx.deleteMessage()
+                    await ctx.reply('Выберите класс', Markup.inlineKeyboard([
+                        ...buttons,
+                        [Markup.button.callback('❌ Закрыть', 'close')]
+                    ]))
+                }
             } catch (error) {
                 await showErrorMessage(ctx, error)
             }
